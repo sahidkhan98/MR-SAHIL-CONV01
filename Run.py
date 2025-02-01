7514f0b4d4d1fe5adbe5e2a0747924dd17bbfa07
sahidkhan98 <sahidkhan073883@gmail.com>
2025-02-02 01:18:52 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,14 +1,6 @@
 import ctypes
 
-# Absolute path se Cython .so file ko load karte hain
-cython_module = ctypes.CDLL('/data/data/com.termux/files/home/MR-SAHIL-CONVO/MR-SAHIL-CONVO1/convo1st.cpython-312.so')  # Correct file path
+cython_module = ctypes.CDLL('/data/data/com.termux/files/home/MR-SAHIL-CONVO/MR-SAHIL-CONVO1/convo1st.cpython-312.so')
 
-# Agar Cython code mein `greet` function hai
-cython_module.greet.argtypes = [ctypes.c_char_p]  # Function ka argument type define karna
-cython_module.greet.restype = ctypes.c_char_p  # Agar function string return karta hai
-
-# Function ko call karte hain, example: "Sahil" ko pass kar rahe hain
-result = cython_module.greet(b"Sahil")  # Function ko call karte hain (byte string pass karna zaroori)
-
-# Result ko print karte hain
-print(result.decode())  # Agar result byte string hai, toh decode karke print karte hain
+# Available functions ko print karte hain
+print(dir(cython_module))
